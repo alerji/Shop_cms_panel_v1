8f0d66cbca2bae8b44cbf9aeb805154c2e98c513
hamid <vhm.alerji@gmail.com>
2024-09-10 17:23:50 +0330

    ihc commit

--- src/views/orders/CustomerList.vue
+++ src/views/orders/CustomerList.vue
@@ -14,6 +14,7 @@
                   class="mb-0 table-outline"
                   hover
                   striped
+                  column-filter
 
                   :items="items"
                   :fields="fields"
